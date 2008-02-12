fb1391e150917d7fc3abf4355fe94cf8849ac6d7
mcl5@sfu.ca <mcl5@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-12 09:55:54 +0000

    Diagrams (wiki/diagrams)   Diagrams updated so they look nicer.   Diagrams re-rendered. Code (branches/workingProject)   Template project posted.

---
+++ branches/workingProject/src/buddyLibrary/BarGraph.java
@@ -0,0 +1,46 @@
+/*
+BarGraph.java
+Brief File Description
+
+Team Triple Threat
+Log:
+02/11/2008 Mark Lauman Created Template
+*/
+
+package buddyLibrary;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import javax.swing.JPanel;
+
+public class BarGraph extends JPanel {
+    
+    private ArrayList<Float> scores; //
+    private BufferedImage disp; //
+
+    public BarGraph() {
+        /*  */
+    }
+    
+// -------------------------------------------------------------- //
+
+    public void render() {
+        /*  */
+    }
+    
+    public void resetAll() {
+        /*  */
+    }
+    
+    public void setScores(float[] values) {
+        /*  */
+    }
+
+// -------------------------------------------------------------- //
+    
+    @Override
+    public void paint(Graphics g) {
+        /*  */
+    }
+}
