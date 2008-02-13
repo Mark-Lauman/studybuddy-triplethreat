2f4a384973d681ff8f7d6d6130aedc2c3338d931
mcl5@sfu.ca <mcl5@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-13 17:45:04 +0000

    Fixed my last commit, so I have not changed the core project.

---
+++ branches/Mark workingProject/src/Core.java
@@ -0,0 +1,44 @@
+/*
+Core.java
+Brief File Description
+
+Team Triple Threat
+Log:
+02/11/2008 Mark Lauman Created Template
+*/
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+public class Core extends JFrame implements ActionListener {
+    
+    private JMenuBar menuBar; //
+    private String user; //
+    
+    public Core() {
+        /*  */
+    }
+
+    public static void main(String[] args) {
+        /*  */
+    }
+    
+// -------------------------------------------------------------- //
+    
+    public String getUser() {
+        /*  */
+        return null;
+    }
+
+    private void runBuddy(String buddyName) {
+        /*  */
+    }
+    
+// -------------------------------------------------------------- //
+    
+    public void actionPerformed(ActionEvent e) {
+        /*  */
+    }
+}
