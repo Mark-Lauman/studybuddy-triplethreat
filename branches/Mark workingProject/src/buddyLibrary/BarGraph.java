bc5253c06da4f80caa9e31ff8b10a681a95da0e4
mcl5@sfu.ca <mcl5@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-15 11:25:21 +0000

    BarGraph class altered some more

--- branches/Mark workingProject/src/buddyLibrary/BarGraph.java
+++ branches/Mark workingProject/src/buddyLibrary/BarGraph.java
@@ -4,15 +4,19 @@ This class displays the data inside it as a bar graph
 
 Team Triple Threat
 Log:
-02/12/2008 Mark Lauman 
+02/14/2008 Mark Lauman Worked on the render and makeBars function
+02/12/2008 Mark Lauman Implemented Functions for no data
 02/11/2008 Mark Lauman Created Template
 */
 
 package buddyLibrary;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 import javax.swing.JPanel;
 
 public class BarGraph extends JPanel {
@@ -25,13 +29,14 @@ public class BarGraph extends JPanel {
         super();
         disp = null;
         scores = null;
+        this.setBackground(Color.WHITE);
     }
     
 // -------------------------------------------------------------- //
 
     private void render(Graphics g) {
         /* Render the graph */
-        System.out.print(this.getHeight() + " " + this.getWidth());
+        
         Graphics2D brush = (Graphics2D) g;
         /* create a BufferedImage that will work well with the screen
          * The image will be the same size as this frame (Hence the calls to
@@ -40,13 +45,112 @@ public class BarGraph extends JPanel {
                         getWidth(), getHeight());
         //Make a brush that will paint to this image (not the screen!)
         brush = (Graphics2D) disp.getGraphics();
+        //draw this panel 1st, but on the image
+        super.paint(brush);
         
         if(scores == null) {
             //If there are no scores to display, just say there is nothing to
             // display
+            brush.setColor(Color.BLACK);
             brush.drawString("No Data Available",
-                    (this.getWidth()/2), (this.getHeight()/2));
+                    (this.getWidth()/2 -46), (this.getHeight()/2 +5));
         }
+        else {
+            //draw the axis of the graph
+            brush.drawLine(15, 15, 15, this.getHeight() -15);
+            brush.drawLine(15, this.getHeight() -15,
+                              this.getWidth() -15, this.getHeight() -15);
+            //Label the axis
+            if(this.getHeight() > 89) {
+                //If the vertical is too small to label, don't label it
+                brush.drawString("S", 4, 25);
+                brush.drawString("C", 4, 38);
+                brush.drawString("O", 4, 51);
+                brush.drawString("R", 4, 64);
+                brush.drawString("E", 4, 77);
+                brush.drawString("S", 4, 90);
+            }
+            brush.drawString("TIME",
+                                this.getWidth() - 45 , this.getHeight() - 2);
+            
+            //get the bars to display
+            ArrayList<Rectangle> barArr = makeBars();
+            
+            for(int i = 0; i < barArr.size(); i++) {
+                brush.fill(barArr.get(i));
+            }
+        }
+    }
+    
+    private ArrayList<Rectangle> makeBars() {
+        /* Makes the Rectangles that will be displayed
+         * in the BarGraph */
+        
+        //If we have more scores than we have pixel columns, then
+        //we will have to average the scores together.
+        //This section combines scores to have at most 1 bar per column
+        ArrayList<Float> data = new ArrayList<Float>(); //The averaged data
+        //The width of each piece of data (can be fractional)
+        float width = ((float)this.getWidth() -33) /scores.length;
+        float colData = 0; //The summed data for the current column
+        int combinedScores = 0; //The amount of scores in this column
+        float currBar = 0; //A running tally of widths added together
+        float max = Integer.MIN_VALUE; //The maximum value in data
+        for(int i = 0; i < scores.length; i++) {
+            //we are iterating through the scores
+            colData += scores[i]; //Add this score to the column data
+            currBar += width; //add the width in, so we know when this
+                              //column is done
+            combinedScores++; //Add 1 score entry to the colum
+            if(currBar >= 1) {
+                //If this column is full
+                data.add(colData/combinedScores); //get the average, for this
+                                                  //column, make it the value
+                                                  //to be displayed
+                if(colData/combinedScores > max)
+                    //If this is bigger than max, it is now max
+                    max = colData/combinedScores;
+                currBar -= (int)currBar;//remove this bar's width from the tally
+                                        //decimals are preserved for accuracy
+                colData = 0; //clear the data sum
+                combinedScores = 0; //clear the scores contributing to the sum
+            }
+        }
+        
+      //A method to print out all of data (for testing purposes)
+        int chars = 0;
+        String current = null;
+        for(int i =0; i < data.size(); i++) {
+            current = data.get(i) + ", ";
+            System.out.print(current);
+            chars += current.length();
+            if(chars > 50){
+                System.out.println();
+                chars = 0;
+            }
+        }
+        System.out.println("\nMax = " + max);
+        
+        //Now to make rectangles of our data!
+        //The rectangles we are returning
+        ArrayList<Rectangle> result = new ArrayList<Rectangle>();
+        //reset the width to be relative to the averaged data
+        width = ((float)this.getWidth() -33) /data.size();
+        int lowerY = this.getHeight() - 16; //The baseline Y value
+        int RangeY = lowerY - 16; //The size of the range of acceptable Y values
+                                  //If a rectangle is RangeY high, its value
+                                  //equals max.
+        
+        
+        
+        for(int i = 0; i < data.size(); i++) {
+            result.add(new Rectangle((int)(i*width +16),
+                                     (int)((data.get(i) / max) * RangeY +16),
+                                     (int)((i+1)*width +16),
+                                     (int)lowerY));
+        }
+        
+        return result;
     }
     
     public void resetAll() {
@@ -66,12 +170,24 @@ public class BarGraph extends JPanel {
 // -------------------------------------------------------------- //
     
     @Override
+    public void repaint(){
+        disp = null;
+        super.repaint();
+    }
+    
+    @Override
     public void paint(Graphics g) {
         /* repaint the graph */
-        if(disp == null) {
-            System.out.println(this.getHeight() + " " + this.getWidth());
+        
+        //does the graph need to be re-rendered?
+        boolean render = (disp == null ||
+                        this.getWidth() != disp.getWidth() ||
+                        this.getHeight() != disp.getHeight());
+        //if so, render it!
+        if(render) {
             render(g);
         }
+        //draw the graph - it takes up the full JPanel
         g.drawImage(disp, 0, 0, null);
     }
 }
--- branches/Mark workingProject/src/testers/TestBarGraph.java
+++ branches/Mark workingProject/src/testers/TestBarGraph.java
@@ -4,6 +4,7 @@ This class is designed to
 
 Team Triple Threat
 Log:
+02/14/2008 Mark Lauman Sperated tests into functions, implemented tests 2-5
 02/12/2008 Mark Lauman Created main, ran test 1
 */
 
@@ -16,16 +17,80 @@ import javax.swing.JFrame;
 
 public abstract class TestBarGraph {
     public static void main(String[] args) {
-        /* testing method */
-        
-        //1st test - No data
-        JFrame test1 = new JFrame("Test 1: No Data");
+        /* run test on BarGraph
+           check functions below for various tests */
+        JFrame test = makeTest3();
+        test.setVisible(true);
+    }
+    
+    private static JFrame makeTest1() {
+        //1st test - No scores, Not resizeable
+        JFrame frame = new JFrame("Test 1: No Scores, Fixed");
         BarGraph graph1 = new BarGraph();
         graph1.setPreferredSize(new Dimension(100, 100));
-        test1.getContentPane().add(graph1);
-        test1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        test1.setResizable(false);
-        test1.setVisible(true);
-        test1.pack();
+        frame.getContentPane().add(graph1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        return frame;
+    }
+    
+    private static JFrame makeTest2() {
+        //2nd test - No scores, resizable
+        JFrame frame = new JFrame("Test 2: No Scores, Resizable");
+        BarGraph graph1 = new BarGraph();
+        graph1.setPreferredSize(new Dimension(80, 80));
+        frame.setContentPane(graph1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        return frame;
+    }
+    
+    private static JFrame makeTest3() {
+        //3rd test - 3 scores, not resizable (bars over 1 pxl wide)
+        JFrame frame = new JFrame("Test 1: 3 Scores, Fixed");
+        BarGraph graph1 = new BarGraph();
+        graph1.setPreferredSize(new Dimension(100, 90));
+        float scores[] = {5, 15, 22};
+        graph1.setScores(scores);
+        frame.setContentPane(graph1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        return frame;
+    }
+    
+    private static JFrame makeTest4() {
+        //4th test - 200 scores, not resizable (bars under pxl wide)
+        JFrame frame = new JFrame("Test 1: 200 Scores, Fixed");
+        BarGraph graph1 = new BarGraph();
+        graph1.setPreferredSize(new Dimension(100, 90));
+        float scores[] = new float[200];
+        for(int i = 0; i < 200; i++) {
+            scores[i] = i;
+        }
+        graph1.setScores(scores);
+        frame.setContentPane(graph1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        return frame;
+    }
+    
+    private static JFrame makeTest5() {
+        //5th test - 200 scores, resizable (variable bars, render times)
+        JFrame frame = new JFrame("Test 1: 200 Scores, Fixed");
+        BarGraph graph1 = new BarGraph();
+        graph1.setPreferredSize(new Dimension(100, 90));
+        float scores[] = new float[200];
+        for(int i = 0; i < 200; i++) {
+            scores[i] = i;
+        }
+        graph1.setScores(scores);
+        frame.setContentPane(graph1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        return frame;
     }
 }
