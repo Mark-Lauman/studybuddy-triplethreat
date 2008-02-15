02cb4b2d2c111ef60b35a132a6d303ea2b0bf134
archon100@hotmail.com <archon100@hotmail.com@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-15 19:02:06 +0000

    made a copy

---
+++ branches/AllanworkingProject/src/buddyLibrary/SoundListener.java
@@ -0,0 +1,30 @@
+/*
+SoundListener.java
+Brief File Description
+
+Team Triple Threat
+Log:
+02/11/2008 Mark Lauman Created Template
+*/
+
+package buddyLibrary;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SoundListener implements ActionListener {
+
+    private AudioClip sound; //
+
+    public SoundListener(AudioClip a) {
+        /*  */
+    }
+
+// -------------------------------------------------------------- //
+    
+// -------------------------------------------------------------- //
+    
+    public void actionPerformed(ActionEvent e) {
+        /*  */
+    }
+}
