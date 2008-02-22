23190d72f23e5e606765a25b115f71d68a0eee92
mcl5@sfu.ca <mcl5@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-22 07:14:44 +0000

    Creating the combined project folder based off of workingProject - THIS IS NOT THE FINAL COMBINATION

---
+++ branches/workingProject2/src/buddyLibrary/Buddy.java
@@ -0,0 +1,52 @@
+/*
+Buddy.java
+Brief File Description
+
+Team Triple Threat
+Log:
+02/11/2008 Mark Lauman Created Template
+*/
+
+package buddyLibrary;
+
+import javax.swing.JPanel;
+
+public class Buddy extends JPanel {
+    
+    public Buddy() {
+        /*  */
+    }
+    
+// -------------------------------------------------------------- //
+    
+    public float[] getStatistics(String buddyName) {
+        /*  */
+    }
+    
+    public String getUser() {
+        /*  */
+    }
+    
+    public JPanel loadImage(String filename) {
+        /*  */
+    }
+    
+    //AudioClip will not work - it is an interface
+    //Find something else
+    public AudioClip loadSound(String filename) {
+        /*  */
+    }
+    
+    //AudioClip will not work - it is an interface
+    //Find something else
+    public JPanel makeAudioPlayer(AudioClip sound) {
+        /*  */
+    }
+    
+    public void setTitle(String title) {
+        /*  */
+    }
+    
+// -------------------------------------------------------------- //
+    
+}
---
+++ branches/workingProject2/src/coreScreens/Stats.java
@@ -0,0 +1,41 @@
+/*
+Stats.java
+Brief File Description
+
+Team Triple Threat
+Log:
+02/11/2008 Mark Lauman Created Template
+*/
+
+package coreScreens;
+
+import buddyLibrary.BarGraph;
+import buddyLibrary.Buddy;
+import buddyLibrary.SelectionMenu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Graphics;
+
+public class Stats extends Buddy implements ActionListener {
+
+    private SelectionMenu menu; //
+    private BarGraph graph;
+    private Buddy buddy;
+
+    public Stats(Buddy b) {
+        /*  */
+    }
+
+// -------------------------------------------------------------- //
+    
+// -------------------------------------------------------------- //
+    
+    @Override
+    public void paint(Graphics g) {
+        /*  */
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        /*  */
+    }
+}
