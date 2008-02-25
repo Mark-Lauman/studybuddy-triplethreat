f77a2de70f310d94a7d92ab8ffad46b2535ecf47
mcl5@sfu.ca <mcl5@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-25 05:18:38 +0000

    Updated SoundPlayer.java to run off the Java Sound API Implemented TestSoundPlayer.java - all tests pass Documented both so they follow our standard

--- branches/workingProject2/src/buddyLibrary/SoundPlayer.java
+++ branches/workingProject2/src/buddyLibrary/SoundPlayer.java
@@ -3,6 +3,7 @@
  * 
  * Team Triple Threat
  * Log:
+ * 02/24/2008 Mark Lauman Re-did SoundPlayer to use up-to-date API's
  * 02/24/2008 Mark Lauman Updated comments so they line up with new standards
  * 02/20/2008 Allan Lei   Updated Audiostream playing
  * 02/14/2008 Allan Lei   Completed working module
@@ -10,95 +11,90 @@
 
 package buddyLibrary;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileInputStream;
+import java.io.IOException;
 import javax.swing.JButton;
-import sun.audio.AudioData;
-import sun.audio.AudioStream;
-import sun.audio.AudioPlayer;
-import sun.audio.ContinuousAudioDataStream;
 
 /**
  * This is a <code>JButton</code> object, that plays an audio clip when clicked.
- * The audio clip must be in .wav format.
+ * The audio clip must be supported by the
+ * <a href="http://java.sun.com/products/java-media/sound/techReference/javasoundfaq.html#formats">
+ * Java Sound API</a>.
  * @author Team Triple Threat
+ * @see <a href="http://java.sun.com/javase/6/docs/api/java/applet/AudioClip.html">
+ *      java.applet.AudioClip</a>
+ * @see <a href="http://java.sun.com/javase/6/docs/api/java/awt/event/ActionListener.html">
+ *      java.awt.event.ActionListener</a>
+ * @see <a href="http://java.sun.com/javase/6/docs/api/java/io/IOException.html">
+ *      java.io.IOException</a>
+ * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/JButton.html">
+ *      javax.swing.JButton</a>
  */
-public class SoundPlayer extends JButton implements ActionListener {
-
-    private AudioStream song;
-    private ContinuousAudioDataStream cas;
-    private AudioData data;
-    private String fName;
-    private Boolean audiostream = false;
-
+public class SoundPlayer extends JButton implements ActionListener{
+    
+    private AudioClip sound;
+    
     /**
      * Constructs a <code>SoundPlayer</code> that points to the file located at
-     * <code>filename</code>. If the file is not a .wav file,
+     * <code>filename</code>
      * @param filename The filename of the sound you wish to play
+     * @throws java.io.IOException If <code>filename</code> is invalid
      */
-    public SoundPlayer(String filename) {
-        super("Play");
-        fName = filename;
-        try {
-            AudioStream as = new AudioStream(new FileInputStream(new File(fName)));
-            data = as.getData();
-            cas = new ContinuousAudioDataStream(data);
-        } catch (Exception ex) {
-            ex.printStackTrace();
+    public SoundPlayer(String filename) throws IOException {
+        super("Play Sound"); // Make this a button with play sound written on it
+        File f = new File(filename); // The File object provides additional
+                                     // features, used below.
+        sound = Applet.newAudioClip(f.toURI().toURL());
+            // lets make our sound object point to filename
+        if(!f.exists()) {
+            //If the file doesn't exist at all
+            throw new IOException("File does not exist");
         }
-
-        setActionCommand(filename);
-        addActionListener(this);
-        validate();
+        if(sound.toString().equals("sun.applet.AppletAudioClip@c40c80")) {
+            //If the file is not supported, then sound always equals the above
+            //string. So throw an exception!
+            throw new IOException("File is not a supported sound");
+        }
+//        System.out.println(sound);
+        this.addActionListener(this);
     }
 
     /**
      * If you have already opened the sound, you may create a new
      * <code>SoundPlayer</code> linked to your sound by passing it to this
-     * function.
-     * @param as An opened sound file.
+     * function
+     * @param audio An opened sound file.
      */
-    public SoundPlayer(AudioStream as) {
-        super("Play");
-        try {
-            data = as.getData();
-            cas = new ContinuousAudioDataStream(data);
-
-            audiostream = true;
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-        setActionCommand(cas.toString());
-        addActionListener(this);
-        validate();
+    public SoundPlayer(AudioClip audio) {
+        super("Play Sound");
+        sound = audio;
+        this.addActionListener(this);
     }
 
     /**
      * Play the audio file linked to this button
      */
     public void play() {
-        AudioPlayer.player.start(cas);
+        sound.play();
     }
     
     /**
-     * If the audio file linked to this button is playing, stop it.
+     * If the audio file linked to this button is playing, stop it
      */
     public void stop() {
-        AudioPlayer.player.stop(song);
+        sound.stop();
     }
 
     /**
-     * Plays the sound. When this button is clicked, this function is called.
-     * @param e
+     * When this button is clicked, this function is called and the sound is
+     * played.
+     * @param e An <code>ActionEvent</code> relating to this button
      */
     public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals(cas.toString())) {
-            try {
-                play();
-            } catch (Exception ex) {
-            }
-        }
+        play();
     }
 }
--- branches/workingProject2/src/testers/TestSoundPlayer.java
+++ branches/workingProject2/src/testers/TestSoundPlayer.java
@@ -10,6 +10,7 @@ package testers;
 
 import buddyLibrary.SoundPlayer;
 import java.awt.Dimension;
+import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -17,6 +18,8 @@ import javax.swing.JPanel;
  * This tests the SoundPlayer class, to prove that it is working properly.
  * @author Team Triple Threat
  * @see <a href="../buddyLibrary/SoundPlayer.html">buddyLibrary.SoundPlayer</a>
+ * @see <a href="http://java.sun.com/javase/6/docs/api/java/io/IOException.html">
+ *      java.io.IOException</a>
  * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/JFrame.html">
  *      javax.swing.JFrame</a>
  * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/JPanel.html">
@@ -27,20 +30,28 @@ public class TestSoundPlayer {
     /**
      * Run a test on SoundPlayer. The test run is determined by the second line
      * in the code. Change the number there to change the test.
-     * @param args arguments passed on command line. These are ignored.
+     * @param args Arguments passed on command line. These are ignored.
      */
     public static void main(String[] args) {
-        JFrame test = makeTest2();
-        test.setVisible(true);
+        try{
+            JFrame test = makeTest1();
+            test.setVisible(true);
+            System.out.println("No errors!");
+        }
+        catch(IOException e) {
+            System.out.println("ERROR - IOException: "
+                                 + e.getMessage());
+        }
     }
     
     /**
-     * The first test, what happens if you pass an invalid file to SoundPlayer's
-     * constructor.
+     * The first test: what happens if you pass an invalid file to
+     * <code>SoundPlayer</code>'s constructor?
      * @return A <code>JFrame</code> containing the first test, ready to be set
      *         visible
+     * @throws java.io.IOException If the file name did not work
      */
-    public static JFrame makeTest1() {
+    public static JFrame makeTest1() throws IOException {
         JFrame frame = new JFrame("Test 1: Invalid Constructor");
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(500, 500));
@@ -53,17 +64,60 @@ public class TestSoundPlayer {
     }
     
     /**
-     * The second test, what happens if you pass a valid file to SoundPlayer's
-     * constructor. You can also check that clicking on the button does play the
-     * sound.
+     * The second test: what happens if you pass a valid file to
+     * <code>SoundPlayer</code>'s constructor? You can also check that clicking
+     * on the button does play the sound.
      * @return A <code>JFrame</code> containing the second test, ready to be set
      *         visible
+     * @throws java.io.IOException If the file name did not work
      */
-    public static JFrame makeTest2() {
+    public static JFrame makeTest2() throws IOException {
         JFrame frame = new JFrame("Test 2: Valid Constructor");
         JPanel panel = new JPanel();
         panel.setPreferredSize(new Dimension(500, 500));
-        panel.add(new SoundPlayer("src/testers/test.wav"));
+        String musicLocation = System.getProperty("user.dir")
+                                + "\\src\\testers\\test.wav";
+        panel.add(new SoundPlayer(musicLocation));
+        frame.getContentPane().add(panel);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        return frame;
+    }
+    
+    /**
+     * The third test: what happens if you pass an invalid file to
+     * <code>SoundPlayer</code>'s constructor? (But the file exists)
+     * @return A <code>JFrame</code> containing the third test, ready to be set
+     *         visible
+     * @throws java.io.IOException If the file name did not work
+     */
+    public static JFrame makeTest3() throws IOException {
+        JFrame frame = new JFrame("Test 3: Invalid file - not sound");
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        String musicLocation = System.getProperty("user.dir")
+                                + "\\src\\testers\\CodingStyleExample.java";
+        panel.add(new SoundPlayer(musicLocation));
+        frame.getContentPane().add(panel);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        return frame;
+    }
+    
+    /**
+     * The fourth test: what happens if you use too short a file extension?
+     * @return A <code>JFrame</code> containing the fourth test, ready to be set
+     *         visible
+     * @throws java.io.IOException If the file name did not work
+     */
+    public static JFrame makeTest4() throws IOException {
+        JFrame frame = new JFrame("Test 3: Valid file, short path");
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 500));
+        String musicLocation = "\\src\\testers\\test.wav";
+        panel.add(new SoundPlayer(musicLocation));
         frame.getContentPane().add(panel);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
