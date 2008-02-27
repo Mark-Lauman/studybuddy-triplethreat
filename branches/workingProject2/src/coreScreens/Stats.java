ad6a5b0ec648a3931d845f5d5e045434e6d1bc4b
vkao@sfu.ca <vkao@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-02-27 17:51:22 +0000

    Add some more comments, and add the border setting on the Stats screen.

--- branches/workingProject2/src/coreScreens/Stats.java
+++ branches/workingProject2/src/coreScreens/Stats.java
@@ -4,6 +4,7 @@
  * 
  * Team Triple Threat
  * Log:
+ * 02/25/2008 Vic Kao added the border and revised some comments
  * 02/23/2008 Vic Kao completed the class
  * 02/11/2008 Mark Lauman Created Template
  */
@@ -12,11 +13,14 @@ package coreScreens;
 
 import buddyLibrary.BarGraph;
 import buddyLibrary.Buddy;
+import buddyLibrary.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import javax.swing.*;
 import java.util.HashMap;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 
 /**
  * This class handles the statistic of each body and displays it with a 
@@ -30,30 +34,36 @@ public class Stats extends Buddy implements ActionListener {
     //HashMap to keep track of the list
     HashMap<JLabel,String> exportmap= new HashMap<JLabel,String>();
     
-    /** the size of the JPanel */
+    /** the width of the JPanel */
     public final int StatsWidth = 400;
+    /** the height of the JPanel */
     public final int StatsHeight = 400;
-    
+    /** the JPanel menu for the left hand side */
     private JPanel menu = new JPanel(); 
+    /** the graph object to be displayed */
     private BarGraph graph;
-    private Buddy buddy;
-    //variables used for the right hand side JPanel
+    //private Buddy buddy;
+    /** the JPanel for the right hand side(bottom part) */
     private JPanel rightScorePanel = new JPanel();
+    /** the JLabel "Scores" used for the score JPanel */
     private JLabel scores = new JLabel();
+    /** the JLabel "High Score" used for the score JPanel */
     private JLabel highScore = new JLabel();
+    /** the JLabel "Lowest Score" used for the score JPanel */
     private JLabel minScore = new JLabel();
+    /** the JLabel "Average Score" used for the score JPanel */
     private JLabel avgScore = new JLabel();
-    /** the size of the score JPanel */
+    /** the width of the score JPanel */
     public final int rightWidth = StatsWidth /2;
-    public final int rightHeight = (int)(StatsHeight /4.5);
-    
-    //the place where stores the buddies's stats, which are stored in text file
+    /** the height of the score JPanel */
+    public final int rightHeight = (int)(StatsHeight /4.5);    
+    /** the place where stores the buddies's stats, which are stored in text file */
     public final String buddyDirText = "./buddies" ;
     
-    /**
-    * Handles the event of the clicked JLabel when clicked
+  /**
+    * Handles the event of a JLabel on the list when clicked
     */
-    private void buddylabelSelected(MouseEvent arg0) {
+    private void buddyLabelSelected(MouseEvent arg0) {
         //get the file name of the buddy but the event source, get teh file name of the buddy
         String buddySelected = exportmap.get(arg0.getSource());
 
@@ -65,7 +75,7 @@ public class Stats extends Buddy implements ActionListener {
                
         //set the scores of the graph
         graph.setScores(tempScore);
-        //tell the graph to update itself, by calling it's repaint function
+        //tell the graph to update itself, by calling its repaint function
         graph.repaint();
         
         //for loop to find the maximun, minimun, and average score
@@ -90,39 +100,47 @@ public class Stats extends Buddy implements ActionListener {
        avgScore.setText("Average Score: " + (int) avg);                
     }
     
+   /**
+    * Constructs a <code>Stats</code> window, and displays the name of each buddy,
+    * It will load the statistic of the selected buddy and display as a bar graph,
+    * and the statistic of the user will be shown as well.
+    */    
     public Stats(Buddy b) {
         super();
         //set the layout type to GridLayout
         setLayout(new GridLayout(1,2, 10, 10));
-        setBorder(BorderFactory.createTitledBorder("Stats"));
+        setBorder(BorderFactory.createTitledBorder("Statistics"));
         
         //add the selection panel
         menu.setLayout(new GridLayout(0,1, 10, 10));
-        //create a panel on the left hand side
+        //create a JPanel on the left hand side
         JPanel leftPanel = new JPanel();
-        
-        leftPanel.add(menu, BorderLayout.NORTH); //makes menu display at the top postion
-        
+        //makes menu display at the top postion
+        leftPanel.add(menu, BorderLayout.NORTH);         
         add(leftPanel);
-        setSize(StatsWidth,StatsHeight);    //set the size of leftPanel
+        //set the size of leftPanel
+        setSize(StatsWidth, StatsHeight);    
+        //set the border to black
+        leftPanel.setBorder(BorderFactory.createLineBorder(Color.black));
         
         //create a panel on the right hand side
         JPanel rightPanel = new JPanel();       
-       rightPanel.setLayout(new BorderLayout());    //set its layout to BorderLayout
+        rightPanel.setLayout(new BorderLayout());    //set its layout to BorderLayout
        
-       //add the score graph on the right hand panel(the upper part)
-       graph = new BarGraph();
-       rightPanel.add(graph,BorderLayout.CENTER);
-     
-       //add the scroe panel(the bottom part)
-       rightPanel.add(rightScorePanel, BorderLayout.SOUTH);
-       rightScorePanel.setSize(rightWidth,rightHeight);
+        //add the score graph on the right hand panel(the upper part)
+        graph = new BarGraph();
+        rightPanel.add(graph,BorderLayout.CENTER);
+        rightPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+        //add the scroe panel(the bottom part)
+        rightPanel.add(rightScorePanel, BorderLayout.SOUTH);
+        rightScorePanel.setSize(rightWidth, rightHeight);
        
-       scores.setText("Scores:");   //set the text to "Scores:"
+        //set the text, "Scores:"
+        scores.setText("Scores:");   
        
-       //set the score panel to BorderLayout type
-       rightScorePanel.setLayout(new BorderLayout());       
-       rightScorePanel.add(scores, BorderLayout.NORTH);
+        //set the score panel to BorderLayout type
+        rightScorePanel.setLayout(new BorderLayout()); 
+        rightScorePanel.add(scores, BorderLayout.NORTH);
         
         JPanel scoreButtom = new JPanel();
         
@@ -132,24 +150,30 @@ public class Stats extends Buddy implements ActionListener {
         scoreButtom.add(minScore);
         scoreButtom.add(avgScore);
         
-        rightScorePanel.add(scoreButtom, BorderLayout.SOUTH);
-        
-        rightScorePanel.setPreferredSize(new Dimension(rightWidth,rightHeight));
-       
+        rightScorePanel.add(scoreButtom, BorderLayout.SOUTH);        
+        rightScorePanel.setPreferredSize(new Dimension(rightWidth, rightHeight));
        
         //read the score data from a text file and display it
         File f = new File(buddyDirText);  //add all buddies to the list and create a directory      
         String[] fileList = f.list(); 
        
         for(int i = 0; i < fileList.length; i++) {
-            if(fileList[i].indexOf(".") < 0) {//if the file doesn't have "."
-               JLabel budyScreen = new JLabel(fileList[i]);
-               budyScreen.addMouseListener(new MouseListener(){ 
+            //if the file doesn't have "."
+            if(fileList[i].indexOf(".") < 0) {
+                JLabel buddyScreen = new JLabel(fileList[i]);
+                //set the border to black
+                buddyScreen.setBorder(BorderFactory.createLineBorder(Color.black));
+                //set the font size to 20
+                Font aaaFont = buddyScreen.getFont();
+                Font font5 = aaaFont.deriveFont(20.0f);
+                buddyScreen.setFont(font5);
+               
+                buddyScreen.addMouseListener(new MouseListener(){ 
 			public void mouseClicked(MouseEvent arg0) {			
 			}
  
 			public void mousePressed(MouseEvent arg0) {
-				buddylabelSelected(arg0);
+				buddyLabelSelected(arg0);
 			}
  
 			public void mouseReleased(MouseEvent arg0) {				
@@ -161,17 +185,24 @@ public class Stats extends Buddy implements ActionListener {
 			public void mouseExited(MouseEvent arg0) {			
 			}});
                         
-                menu.add(budyScreen); //add the names to list                       
-                exportmap.put(budyScreen, buddyDirText + "/" + fileList[i]);
+                menu.add(buddyScreen); //add the names to list                       
+                exportmap.put(buddyScreen, buddyDirText + "/" + fileList[i]);
            }
            else {
-                JLabel budyScreen=new JLabel(fileList[i].substring(0, fileList[i].indexOf(".")));
-                budyScreen.addMouseListener(new MouseListener(){ 
+                JLabel buddyScreen = new JLabel(fileList[i].substring(0, fileList[i].indexOf(".")));
+                //set the border to black
+                buddyScreen.setBorder( BorderFactory.createLineBorder(Color.black) );
+                //set the font size to 20.0
+                Font aaaFont = buddyScreen.getFont();
+                Font font3 = aaaFont.deriveFont(20.0f);
+                buddyScreen.setFont(font3);
+                
+                buddyScreen.addMouseListener(new MouseListener(){ 
 			public void mouseClicked(MouseEvent arg0) {			
 			}
  
 			public void mousePressed(MouseEvent arg0) {
-				buddylabelSelected(arg0);
+				buddyLabelSelected(arg0);
 			}
  
 			public void mouseReleased(MouseEvent arg0) {				
@@ -183,8 +214,8 @@ public class Stats extends Buddy implements ActionListener {
 			public void mouseExited(MouseEvent arg0) {			
 			}});
                         
-                menu.add(budyScreen); //add the names to list
-                exportmap.put(budyScreen, buddyDirText + "/" + fileList[i]);
+                menu.add(buddyScreen); //add the names to list
+                exportmap.put(buddyScreen, buddyDirText + "/" + fileList[i]);
            }           
        }
         
@@ -225,7 +256,7 @@ public class Stats extends Buddy implements ActionListener {
 
                         
 			f.add(s);
-                        f.setSize(400, 600);
+                        f.setSize(600, 600);
                         
                         
 			f.setVisible(true);
--- branches/workingProject2/src/coreScreens/UserSelection.java
+++ branches/workingProject2/src/coreScreens/UserSelection.java
@@ -3,15 +3,12 @@
  * 
  * Team Triple Threat
  * Log:
- * 02/25/2008 Allan Lei Added deleteDir for deleting folders with hidden files
  * 02/21/2008 Vic completed the class
  * 02/11/2008 Mark Lauman Created Template
  */
 
-package coreScreens;
+package buddyLibrary;
 
-import buddyLibrary.*;
-import core.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
@@ -27,16 +24,17 @@ import java.util.ArrayList;
  * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/JPanel.html">
  *      javax.swing.JPanel</a>
  */
-public class UserSelection extends buddyLibrary.SelectionMenu{
+public class UserSelection extends buddyLibrary.SelectionMenu {
    /** the space between buttons */   
    public final int subButtonwidth = 50 ;
-   /** the string names for subbuttons and the main button */
+   /** the string name for subbutton "New Player" */
    public final String newPlayerText = "New Player";
+   /** the string name for subbutton "Delete Player" */
    public final String deletePlayerText = "Delete Player";
+   /** the string name for subbutton "Login" */
    public final String loginUserText = "Login";
    /** the directory to store user's data */
-   //public final String userDIR = "./data/user";
-   public final String userDIR = "./build/classes/Data/Users";
+   public final String userDIR = "./data/user";
    /** an array list to store usernames */
    public ArrayList<String> userList = new ArrayList<String>();
    
@@ -48,8 +46,9 @@ public class UserSelection extends buddyLibrary.SelectionMenu{
     * for checking if a user exists already.
     * Also, it creates sub buttons and main button.
     */
-   public UserSelection (int width, int height, Core c) {
-       super(width, height, c);
+   public UserSelection (int width, int height) {
+       super(width, height);
+       setBorder(BorderFactory.createTitledBorder("Study Buddies"));
        
        File f = new File(userDIR);  //add all users to the list and create the directory      
        String [] fileList = f.list();       
@@ -59,7 +58,8 @@ public class UserSelection extends buddyLibrary.SelectionMenu{
        }
        
        //add sub button - New Player
-       addSubButton(newPlayerText, subButtonwidth); 
+       addSubButton(newPlayerText, subButtonwidth);
+       
        //add sub button - Delete Player
        addSubButton(deletePlayerText, subButtonwidth);
        //add the main button - Login
@@ -98,7 +98,7 @@ public class UserSelection extends buddyLibrary.SelectionMenu{
    private void deleteUser(){
        String userTobeDeleted = getSelection();
        
-       //if nothing is chosen on the list, then show the warning message
+       //if nothing is selected on the list, then show the warning message
        if (userTobeDeleted == null ) {
            JOptionPane.showMessageDialog(null,
                    "Please select one user from the list!", "Warning",
@@ -114,29 +114,15 @@ public class UserSelection extends buddyLibrary.SelectionMenu{
                             "Delete Player", JOptionPane.YES_NO_OPTION);
        
        // if the user decides to delete the user, n = 0
-       // and remove the user from the list and delete the directory as well
+       // then remove the user from the list and delete the directory as well
        if(n == 0) {
-           
-           
             list.removeElement(userTobeDeleted);
             userList.remove(userTobeDeleted.toUpperCase());
          File f = new File(userDIR + "/" + userTobeDeleted);
-         deleteDir(f);
+            f.delete();
         }       
    }
-   //http://javaalmanac.com/egs/java.io/DeleteDir.html
-     public static boolean deleteDir(File dir) {
-        if (dir.isDirectory()) {
-            String[] children = dir.list();
-            for (int i=0; i<children.length; i++) {
-                boolean success = deleteDir(new File(dir, children[i]));
-                if (!success) {
-                    return false;
-                }
-            }
-        }
-        return dir.delete();
-    }
+
 
   /**
     * Events for each button
@@ -150,8 +136,11 @@ public class UserSelection extends buddyLibrary.SelectionMenu{
            deleteUser();
        }
        else if(e.getActionCommand().compareTo(loginUserText) == 0) {
-            
+      //if the comand is login
+       //call function to handl user choice
        }
    }
     
+   
+   
 }
