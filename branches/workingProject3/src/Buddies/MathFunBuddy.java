76d84c745a103f20a9ba8300af4fd2a8dd8716e6
vkao@sfu.ca <vkao@sfu.ca@055570e5-a743-0410-9b42-ef75abcc01ea>
2008-03-18 11:24:54 +0000

    Vic Kao finished implementing the MathFunBuddy.java

--- branches/workingProject3/src/Buddies/MathFunBuddy.java
+++ branches/workingProject3/src/Buddies/MathFunBuddy.java
@@ -3,22 +3,22 @@
  * 
  * Team Triple Threat
  * Log:
- * 03/17/2008 Vic Kao updated the database for qustions and pictures needed
+ * 03/17/2008 Vic Kao updated the database and implemented functions in detail
  * 03/16/2008 Vic Kao implemented the structure
  */
 
 package Buddies;
 
 import buddyLibrary.*;
-import java.awt.event.ActionEvent;
-import javax.swing.*;
-import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
 
 /**
- * This Math buddy has a database of math questions related to daily life 
+ * This Math buddy has a database of 20 math questions related to daily life 
  * to help Grade 6 students learn and improve their math skills.
  * @author Team Triple Threat
  * @see <a href="http://java.sun.com/javase/6/docs/api/javax/swing/JPanel.html">
@@ -48,35 +48,47 @@ public class MathFunBuddy extends Buddy implements ActionListener {
     /** the JLabel for displaying the current score */
     private JLabel score;
     /** the label for displaying description of the question */
-    private JLabel questionText;
-    //private JButton AnswerButton;
+    private JTextArea questionText;
+    /** the JButtons used to get the answer*/
+    private JButton AnswerButton;
     /** the width for questionText */
     int questionTextWidth = 100;
     /** the width for questionText */
-    int questionTextHight = 100;
-    
+    int questionTextHight = 50;
+    /** the counter used to record number of questions conseuctively answered correctly */
+    int conseuctiveCorrectCounter = 0;
+    /** the counter used to record number of questions conseuctively answered incorrectly */
+    int conseuctiveIncorrectCounter = 0;
    
     /** the JButtons - "Next" to go to the next question */
     private JButton nextButton;
     /** the JLabel for displaying whether the user got it correct or wrong */
-    private JLabel correctOrWrongLabel;
+    private JTextArea correctOrWrongLabel;
     /** the variable to keep record the score */
     private int scoreGained;
     /** the ArrayList for storing the question names */
-    private ArrayList<String> QuestionNames;
+    private ArrayList<String> questionNames;
     /** the variable acting as a pointer, pointing the current question */
-    private int playgameIndex;
-
+    private int questionIndex;
+    /** set the easy level */
+    private final int easyLvl = 1;
+    /** the ArrayList to store the questions that are not played yet */
+    private ArrayList<Integer> notPlayedYet = new ArrayList<Integer>();
+    /** the variable to what the current question is */
+    private int currentQuestion;//record the # of the question of the game
+    
     
    /**
      * Constructs a <code>MathFunBuddy</code> buddy, and loads the welcome screen
      * and question screen.
      */
     public MathFunBuddy (){
+        this.setLayout(new BorderLayout());
         createStartPanel();
         add(startPanel);                
         
         createQuestionPanel();
+        
     }
     
     /**
@@ -92,7 +104,8 @@ public class MathFunBuddy extends Buddy implements ActionListener {
         this.repaint();
         validate();
         
-        //startGame();
+        //start the game
+        startGame();
     }
     
     /**
@@ -100,54 +113,92 @@ public class MathFunBuddy extends Buddy implements ActionListener {
      */
     private void startGame()
     {
-        //create QuestionNames
-        
-        //open the file for a list of question names
-        //while it is not the end of file
-            //get a line from the file
-            //add it to the QuestionNames
-        
-        //record that it is play the first game
-        playgameIndex=0;
-        
-        //initialize score
-        scoreGained=0;
-        
-        playGame(playgameIndex);
+        //set currentQuestion to 0
+        currentQuestion = 0;
+        //set nextButton and correctOrWrongLabel to false
+        nextButton.setVisible(false);
+        correctOrWrongLabel.setVisible(false);
+        try{
+            //create an instance of String for QuestionNames 
+            questionNames = new ArrayList<String>();
+
+            //open the file for a list of question names
+            File f = new File("./questions/questionRecord.txt");
+            
+            Scanner scan = new Scanner(f);
+
+            //while it is not the end of file
+            while(scan.hasNext()){
+                //get a line from the file and add it to the QuestionNames
+                questionNames.add(scan.nextLine());
+            }
+            
+            //random generate the first question of easy level
+            Random generator = new Random();
+            questionIndex = generator.nextInt(easyLvl);
+
+            //initialize score to 0
+            scoreGained = 0;
+            //set the score text
+            score.setText("Score: " + scoreGained);
+
+            //add all questions to the notPlayedYet ArrayList
+            for(int i = 0; i < questionNames.size(); i++){
+                notPlayedYet.add(i);
+            }          
+            
+            //play the game
+            playGame(questionIndex);
+            }catch (Exception e){
+                System.out.println(e);                 
+            }
     }
+    
     
     /**
      * Play the game by passing the gameIndex, the current question
-     * @param gameIndex
+     * @param gameIndex The index of the current question, like "Question 1"
      */
     private void playGame(int gameIndex)
     {
+        //remove the current question from notPlayedYet ArrayList
+        notPlayedYet.remove(new Integer(gameIndex));        
+        
+        //update the question label by 1
+        currentQuestion++;
+        question.setText("Question: " + currentQuestion);
+        
         //get the game name with the game index
-        //open the "question.txt" in the dirctor of the game
+        String currentQuestionName = questionNames.get(gameIndex);
+        //open the "question.txt" in the directory of the currentQuestionName
+        File f = new File("./questions/"+ currentQuestionName+"/question.txt");
         
-        //for i = 0 to 2
-        //create the image icon with img0.png
-        //set choiceButtons[i]'s icon to the icon created
+        try{
+            Scanner scan = new Scanner(f);
         
-        //create the image icon with img0alternative.png
-        //set choiceButtons[1]'s rollover icon to the icon created
+            //for loop to set images and text on the buttons
+            for(int i = 0; i < 3; i++){
+                choiceButtons[i].setIcon(new ImageIcon("./questions/"+ currentQuestionName+ "/img" + i + ".jpg"));
+                choiceButtons[i].setRolloverIcon(new ImageIcon("./questions/"+ currentQuestionName+ "/img" +  i + "a.jpg"));
+                choiceButtons[i].setRolloverEnabled(true); 
+                //get the a line from the text file
+                choiceButtons[i].setText(scan.nextLine());
+                //set the choiceButtons[0]'s text 
+                choiceButtons[i].setVerticalTextPosition(JButton.BOTTOM);                
+            } 
+            
+            //get a line from the .txt file as instruction
+            questionText.setText(scan.nextLine());
         
-        //get the a line from the text file
-        //set the choiceButtons[0]'s text 
-                
-                
-        //get a line from the .txt file as instruction
-        
-        //get a integer as the index value of the answer
-        //record the button to the answer button
-        //AnswerButton=choiceButtons[];
-        
-        
-        
+            //get a integer as the index value of the answer
+            AnswerButton = choiceButtons[scan.nextInt()];
+            } catch (Exception e){
+                System.out.println(e);
+            }        
     }
     
-    /** create the start panel
-     * 
+    /** 
+     * Create the start panel
      */
     private void createStartPanel()
     {
@@ -162,35 +213,29 @@ public class MathFunBuddy extends Buddy implements ActionListener {
         //buttom panel for displaying the "START" buttom
         JPanel buttomPanel = new JPanel();
         
-        //set the sizze of starting panel
         startPanel.setSize(700, 700);        
         //set the layout to BorderLayout
         startPanel.setLayout(new BorderLayout());
 
         
-        
         //add the label to the upper panel
         upperPanel.add(introJLabel);
-//set the size of the upper panel
-//upperPanel.setPreferredSize(new Dimension(100, 40));
+
+//tesing purpose
 upperPanel.setBackground(Color.WHITE);        
-        
 
         //set the size of the buttom panel
-        buttomPanel.setPreferredSize(new Dimension(100, 100));
-          
+        buttomPanel.setPreferredSize(new Dimension(100, 100));   
         
         //create the button, "START"
-        startButton = new JButton("Start");
+        startButton = new JButton("START!");
         //set the preferred size of the button
         startButton.setPreferredSize((new Dimension(startButtonWidth, startButtonHeight)));
         //set the event listenter to this 
         startButton.addActionListener(this);
         
-        
         //add the button to the buttom panel
-        buttomPanel.add(startButton);
-        
+        buttomPanel.add(startButton);        
         
         //add the upperPanel and buttomPanel to the main panel (startPanel)
         startPanel.add(upperPanel,BorderLayout.CENTER);
@@ -201,9 +246,12 @@ upperPanel.setBackground(Color.WHITE);
      * Creates the question panel
      */
     private void createQuestionPanel()
-    {   questionPanel=new JPanel();//points to a new JPanel
+    {   
+        
+        questionPanel=new JPanel();
+        //set its layout to BorderLayout
         questionPanel.setLayout(new BorderLayout());
-//set the background color
+//tesing purpose: set the background color
 questionPanel.setBackground(Color.GREEN);
         //set the size of question panel
         questionPanel.setSize(700,660);
@@ -213,11 +261,12 @@ questionPanel.setBackground(Color.GREEN);
         //set it's layout to bordered
         upperPanel.setLayout(new BorderLayout());
         //create the question label
-        question = new JLabel("Question 1");
+        question = new JLabel();
         //set the preferred size
         question.setPreferredSize(new Dimension(100, 50));
         //add it to the west of upper panel
         upperPanel.add(question, BorderLayout.WEST);
+//tesing purpose: set the background color        
 upperPanel.setBackground(Color.MAGENTA);
         
         //create the socre label
@@ -233,23 +282,31 @@ upperPanel.setBackground(Color.MAGENTA);
         
         //create the middle panel of the question panel
         JPanel middlePanel = new JPanel();
-middlePanel.setBackground(Color.cyan); 
-        //set it's layout to bordered
+//tesing purpose: set the background color        
+middlePanel.setBackground(Color.pink); 
+        //set its layout to BorderLayout
         middlePanel.setLayout(new BorderLayout());
-        //questioText points to a new JLabel
-        questionText = new JLabel("Question description goes here......");
-        //set the preferred size of questioText
-        questionText.setPreferredSize(new Dimension(questionTextWidth, questionTextHight));      
-        //add the questioText to the middle pane's north
-        middlePanel.add(questionText, BorderLayout.NORTH);
+        //questionText points to a new JTextArea
+        questionText = new JTextArea();
+//set the preferred size of questionText
+// questionText.setPreferredSize(new Dimension(questionTextWidth, questionTextHight));  
+        //set questionText uneditable and the background to transparent
+        questionText.setEditable(false);
+        questionText.setBackground(new Color(255,255,255, 100));
+        //let questionText break line automatically
+        questionText.setOpaque( false );
+        questionText.setLineWrap(true);
+        questionText.setWrapStyleWord(true);
         
+        //add the questioText to the middle panel's north
+        middlePanel.add(questionText, BorderLayout.NORTH);
         
         //create a choice button panel
         JPanel choiceButtonPanel = new JPanel();
-        //set it's layout to gridbag layout, to 1 row 3 columns
+        //set its layout to GridLayout, 1 row 3 columns
         choiceButtonPanel.setLayout(new GridLayout(1,3));
         
-        //create the 3 choices buttons by looping                
+        //create the 3 choices buttons by for loop
         for (int i = 0; i <  choiceButtons.length; i++){
             choiceButtons[i] = new JButton("Choice " + (i + 1));
             //set button's event listener to this class
@@ -269,44 +326,166 @@ middlePanel.setBackground(Color.cyan);
         
         //create the lower panel of the question panel
         JPanel lowerPanel = new JPanel();
+//tesing purpose: set the background color           
 lowerPanel.setBackground(Color.orange);
-        //set it's layout to gridbag layout, 2 rows , 1 column
-        lowerPanel.setLayout(new GridLayout(2,1));
+        //set its layout to GridLayout, 1 row , 2 column
+        lowerPanel.setLayout(new GridLayout(1,2));
         //create the correct-or-wrong label
-        correctOrWrongLabel = new JLabel("CORRECT ANSWER!!");
-        correctOrWrongLabel.setPreferredSize(new Dimension(100,90));
+        correctOrWrongLabel = new JTextArea();
+//correctOrWrongLabel.setPreferredSize(new Dimension(100,90));
+        //set correctOrWrongLabel uneditable and the background to transparent
+        correctOrWrongLabel.setEditable(false);
+        correctOrWrongLabel.setBackground(new Color(255,255,255, 100));
+        //let correctOrWrongLabel break line automatically by word
+        correctOrWrongLabel.setOpaque( false );
+        correctOrWrongLabel.setLineWrap(true);
+        correctOrWrongLabel.setWrapStyleWord(true);        
         //add it to the panel
         lowerPanel.add(correctOrWrongLabel);
         
         //create the next button
         nextButton = new JButton("Next");
         //set it's preferred size
-        //nextButton.setPreferredSize(new Dimension(60,50));
+
         //add it to the panel
         lowerPanel.add(nextButton);
         nextButton.addActionListener(this);
          
-        lowerPanel.setPreferredSize(new Dimension(10,100));
+        lowerPanel.setPreferredSize(new Dimension(50,50));
         //add the panel to the queston panel's south
         questionPanel.add(lowerPanel, BorderLayout.SOUTH);
     }
     
-
+    /**
+     * This function handles the event when the user answers correctly
+     */
+    public void answerCorrectquestion()
+    {
+        String msgCorrect = "Excellent! You have gained 10 points! :)";
+        correctOrWrongLabel.setText(msgCorrect);
+        //update the score by 10 points
+        scoreGained =+10;
+        score.setText("Score: " + scoreGained);
+        //increment the conseuctiveCorrectCounter
+        conseuctiveCorrectCounter++;
+        //reset conseuctiveIncorrectCounter to 0
+        conseuctiveIncorrectCounter = 0;
+    }
     
-      public void actionPerformed(ActionEvent e) 
+    /**
+     * This function handles the event when the user answers incorrectly
+     */    
+    public void answerWrongquestion()
+    {
+        String msgWrong = "Sorry, your answer was not correct. " +
+                "Maybe you missed some details? Try harder for the next one! :)";
+        correctOrWrongLabel.setText(msgWrong);
+        //increment the conseuctiveIncorrectCounter
+        conseuctiveIncorrectCounter++;
+        //reset conseuctiveCorrectCounter to 0
+        conseuctiveCorrectCounter = 0;
+    }
+
+      
+      /**
+       * This function handles when the user finishes playing the buddy
+       */
+      public void finishPlayingBuddy()
       {
-          //if the startbutton clicked
-          if(e.getSource() == startButton)
-            startButtonClicked();
           
       }
       
+      /**
+       * This function handles the event when "Next" button is clicked
+       */
+      public void nextButtonClicked(){
+          
+          //decide which question to display next
+          updateQuestionIndex();
+          
+          //re-enable all choice buttoms
+          for(int i = 0; i < choiceButtons.length; i++)
+              choiceButtons[i].setEnabled(true);
+          
+          
+          //make correctorwronglable and nextbutton invisible
+          correctOrWrongLabel.setVisible(false);
+          nextButton.setVisible(false);
+          
+          //play the next game
+          playGame(questionIndex);
+      }
+      
+      /**
+       * This function updates the current question index. If the question showed 
+       * up before, it will not show up again. Instead, it will look for unused
+       * questions.
+       */
+      public void updateQuestionIndex(){
+          
+          int i = 0;
+          //find the location in notPlayedYet that is larger than questionIndex
+          for(; i < notPlayedYet.size(); i++){
+              if(notPlayedYet.get(i) > questionIndex)
+                  break;
+          }
+          
+          //if the user answers correctly 1 question conseuctively
+          //then the next question would be harder
+          if(conseuctiveCorrectCounter >= 3)
+              questionIndex = notPlayedYet.get(i+1);          
+          else{
+              //else the next question will be eaiser
+              //if easier question is available
+              if ( (i-1) >=0)
+                  questionIndex = notPlayedYet.get(i-1); //roll back the question by 1
+              else
+                  questionIndex = notPlayedYet.get(i); //no change
+          }
+      }
+      
+      /**
+       * This function interactes with the user and handles the events when
+       * some particular actions are performed
+       * @param e The action performed by the user
+       */
+      public void actionPerformed(ActionEvent e) {
+          //the user clickes "START" button on the welcome screen
+          if(e.getSource() == startButton)
+            startButtonClicked();
+          //the user makes decision
+          if(e.getSource() == choiceButtons[0] ||e.getSource() == choiceButtons[1]
+                  || e.getSource() == choiceButtons[2]){
+              if(e.getSource() == AnswerButton)                   
+                  answerCorrectquestion();  //the user answers correctly
+              else
+                   answerWrongquestion();   //the user answers incorrectly
+                            
+              //disable the choice buttons after the user answered
+              for(int i = 0; i < choiceButtons.length; i++)
+                  choiceButtons[i].setEnabled(false);
+              
+              //if the user have played 10 questioins, end the program
+              if (currentQuestion == 10)
+                finishPlayingBuddy();
+              
+              //set correctOrWrongLabel and nextButton to visible
+              correctOrWrongLabel.setVisible(true);
+              nextButton.setVisible(true);
+          }
+          //the user clicks the "Next" button
+          if(e.getSource() == nextButton)
+              nextButtonClicked();
+      }
+      
+      //testing codes
       public static void main(String args[]){
           JFrame test = new JFrame();
 
           test.setSize(700,700);
           test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE  );
           test.add(new MathFunBuddy());
+          //test.pack();
           test.setVisible(true);
       }
       
